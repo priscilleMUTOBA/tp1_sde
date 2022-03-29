bac9d2848779346c0c0d134723379b83a35e35be
Priscille MUTOBA <101174663+priscilleMUTOBA@users.noreply.github.com>
2022-03-29 11:02:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#ex1
+var1 = "priscille mutoba"
+basic.show_string(var1)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
+
+#ex1
 def on_forever():
     led.plot(3, 2)
     basic.pause(200)
