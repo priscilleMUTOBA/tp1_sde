05551508d1f12bdf6495e953d2ea6b0e69c5e9b0
Priscille MUTOBA <101174663+priscilleMUTOBA@users.noreply.github.com>
2022-03-29 10:57:00 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+def on_forever():
+    led.plot(3, 2)
+    basic.pause(200)
+    led.unplot(3, 2)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function on_forever() {
+    led.plot(3, 2)
+    basic.pause(200)
+    led.unplot(3, 2)
+})
